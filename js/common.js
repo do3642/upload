d39326e7b6f661a179bc9e3d78d51a8ba1aea43b
soljilee <59036713+soljilee@users.noreply.github.com>
2020-05-28 18:01:48 +0900

    0528 반응형 스크립트
    반응형 스크립트
    hasClass,localStroge사용해서 묶음
    
    및 네비 클래스 ,스크롤반응 등등

--- js/common.js
+++ js/common.js
@@ -1,32 +1,50 @@
 window.addEventListener('DOMContentLoaded',function(){
-  
 
+    
+    
+//모바일 환경 dt클릭시 dd 슬라이드 
+    function abTxt(){ 
+        $('.aboutTxt dl dt').on('click',function(){
+            if(!$('body').hasClass('control')){
+                $('.aboutTxt dl dd').slideToggle();
+           
+            }
+           
+           
+        })
+    }
+
+  //버거메뉴
     function bugger(){
         $('.bugger').on('click',function(){
             $(this).toggleClass('active')
             $(this).toggleClass('Hover')
             if($('.bugger').hasClass('active')){
-                $('nav ul').slideDown();
+                $('nav div').eq(0).slideDown();
+             
             }else{
-                $('nav ul').slideUp();
+                $('nav div').eq(0).slideUp();
             }
         });
     }
     bugger();
-    var intro = document.querySelector('.intro');
+  
 
     // 인트로 스크롤 락
     function lock(){
         $('.intro').on('scroll touchmove mousewheel', function(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            return false;
+            if($('body').hasClass('control')){
+                event.preventDefault();
+                event.stopPropagation();
+                return false;
+            }
           });
          
     }
    
     lock();
     
+    //클릭시 스크롤시 해당 offset으로 이동 함수
     function movescroll(ele,time){
         
         var move = $(ele).offset().top;
@@ -37,7 +55,7 @@ window.addEventListener('DOMContentLoaded',function(){
     }
  
 
-    //인트로 버튼 클릭시 캔버스 활성화/스크롤 이동
+    //인트로 버튼 클릭시 바탕채우기 활성화/스크롤 이동
     function introbtn(i){
         $('.btn').on('click',function(){
 
@@ -61,6 +79,7 @@ window.addEventListener('DOMContentLoaded',function(){
     introbtn();
  
 
+    //네비버튼 클릭시 해당 텍스트 감지하여 스크롤 이동
     function navbtn(){
         $('nav li a').on('click',function(e){
             e.preventDefault();
@@ -94,12 +113,44 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
 
-
+    //윈도우 스크롤 감지 이벤트 모음
     function navscroll(){
+        var sTop;
         $(window).on('scroll',function(){
-            var sTop = $(this).scrollTop();
+            sTop = $(this).scrollTop();
+            var homeTop = $('.intro').offset().top;
             var conTop = $('.container').offset().top;
-            console.log(conTop);
+            var portTop = $('.port').offset().top;
+            var abilityTop = $('.ability').offset().top;
+            var last = $('.contact').offset().top;
+           
+            
+            
+          
+            
+        $('.scrollEvent').each(function(i){
+            
+            var con = $('.scrollEvent').eq(i).offset().top;
+            if(sTop >con-500){
+                $('nav li').removeClass('active');
+                $('nav li').eq(i).addClass('active');
+                console.log(i);
+            }else{
+              
+            }
+          
+        
+        });
+
+
+
+
+
+
+
+
+
+
             if(sTop < conTop){
                 $('nav').css({
                     position:'absolute'
@@ -108,7 +159,24 @@ window.addEventListener('DOMContentLoaded',function(){
                 $('nav').css({
                     position:'fixed'
                 })
+                $('.port >span').addClass('active');
+                $('.port').removeClass('active');
+                $('.port').css({width:'100%'});
+                $('.port div ul').eq(1).addClass('active');
             }
+
+            if(sTop > last-600){
+                $('.tri p').addClass('active');
+            }else{
+                $('.tri p').removeClass('active');
+            }
+
+            if(sTop > portTop-200){
+              
+            }else{
+                portReverse2()
+            }
+
         });
     }
     navscroll();
@@ -119,22 +187,26 @@ window.addEventListener('DOMContentLoaded',function(){
     function floor(){
         var floorBln = true;
 
-        $('.floor').on('mouseenter',function(){
-            $(this).addClass('active');
+        $('.floor').on('mouseenter click',function(){
+            $(this).addClass('active'); // 원에 에니메이션 줌
            
-            setTimeout(function(){
+            setTimeout(function(){ //원 애니가 2초 임으로 2초 뒤에 해당 실행
                 $('.aboutTxt dl').slideDown(1500);
                 setTimeout(function(){
                     $('nav').slideDown();
+                    
                     $('.aboutTxt dl').css({
                         transition:'2s'
                     });
 
                     $('.about').on('wheel',function(e){
                         var delta = e.originalEvent.wheelDelta;
-                        e.preventDefault();
-                        e.stopPropagation();
-                        
+                        if($('body').hasClass('control')){
+                            e.preventDefault();
+                            e.stopPropagation();
+                        }
+                      
+                    
                         if(floorBln) {
                             floorBln = !floorBln
                             
@@ -155,7 +227,9 @@ window.addEventListener('DOMContentLoaded',function(){
                             }//엘스문
 
                             if($('.aboutTxt dl').hasClass('active')){
-                                $('.port').addClass('active');
+                                if($('body').hasClass('control')){
+                                    $('.port').addClass('active');
+                                }
                             }else{
                                 $('.port').removeClass('active');
                             }
@@ -178,11 +252,11 @@ window.addEventListener('DOMContentLoaded',function(){
         })
 
         $('.about').on('wheel',function(e){
-            var delta = e.originalEvent.wheelDelta;
-            e.preventDefault();
-            e.stopPropagation();
-           
-        
+            if($('body').hasClass('control')){
+                e.preventDefault();
+                e.stopPropagation();
+            }
+          
  
         });
     }
@@ -219,25 +293,38 @@ window.addEventListener('DOMContentLoaded',function(){
 
     //포트폴리오 페이지에서 올라갈때
     function portReverse2(){
-        $('.port').addClass('active');
+        if($('.aboutTxt dl').hasClass('active')){
+            if($('body').hasClass('control')){
+                $('.port').addClass('active');
+            }
+        }
+       
         $('.port').css({width:'70%'});
         $('.port >span').removeClass('active');
         $('.port div ul').eq(1).removeClass('active');
+        
     }
     function portReverse(){
         $('.port').on('wheel',function(e){
-            var delta = e.originalEvent.wheelDelta;
-            e.preventDefault();
-            e.stopPropagation();
-            if(delta <0){
+            if($('body').hasClass('controlPort')){
 
-                movescroll('.ability',500);
+                var delta = e.originalEvent.wheelDelta;
+                e.preventDefault();
+                e.stopPropagation();
+                if(delta <0){
+    
+                    movescroll('.ability',500);
+    
+                }else{
+                  
+                 movescroll('.container',500);
+                portReverse2();
+                }//엘스문
 
-            }else{
-              
-             movescroll('.container',500);
-            portReverse2();
-            }//엘스문
+
+
+            }
+           
           
  
         });
@@ -247,15 +334,19 @@ window.addEventListener('DOMContentLoaded',function(){
 
 
 
+    var idx = 0;
     function ability3D(){
-
+      
       var x = 1200;
       var bln=true;
-      var idx = 0;
+     
+         
         $('.ability').on('wheel',function(e){
+            if($('body').hasClass('control')){
             e.preventDefault();
             e.stopPropagation();
             
+              
             var delta = e.originalEvent.wheelDelta;
             if(bln) {
                 bln = !bln;
@@ -268,7 +359,7 @@ window.addEventListener('DOMContentLoaded',function(){
                         idx = $(".trans li").length-1;
                         $('.tri p').addClass('active');
                     }
-                   
+                  
                    
                 }else{ //휠 올렸을 때
                     idx--;
@@ -287,6 +378,7 @@ window.addEventListener('DOMContentLoaded',function(){
 
                 $(".trans li").css({
                     opacity: 0.2
+                    
                 })
 
                 $(".trans li").eq(idx).css({
@@ -298,11 +390,36 @@ window.addEventListener('DOMContentLoaded',function(){
                     bln = !bln;
                 },500);
             }
-
+        }
         });
+   
 
     }
-    ability3D();
+
+    function abilityMob(){
+        $('.trans ul').css({
+            transform: 'rotateY(0) translateY(0) translateZ(0)translateX(0)'  
+        });
+        $(".trans li").css({
+            opacity: 1
+        });
+    }
+    function abilityDesk(){
+        $('.trans ul').css({
+            transform: 'rotateY(25deg) translateY(200px) translateZ(-100px)translateX(-1200px)'
+        });
+         $(".trans li").css({
+            opacity: 0.2
+        })
+
+        $(".trans li").eq(idx).css({
+            opacity: 1
+        });
+    }
+    
+
+
+
 
     function contact(){
 
@@ -319,6 +436,127 @@ window.addEventListener('DOMContentLoaded',function(){
     contact();
     
 
+    
+
+
+
+
+//모바일 반응형 제어 스크립트
+
+    function localControl(){
+        if(!localStorage.cheak==1){
+            localStorage.cheak=1;
+            ability3D();
+            abTxt();
+        }
+    }
+    localStorage.clear();
+    localControl();
+    var m550 = window.matchMedia("screen and (max-width: 550px)");
+    var m = window.matchMedia("screen and (max-width: 860px)");
+    var m1200 = window.matchMedia("screen and (max-width: 1200px)");
+
+
+m550.addListener(function(e){
+
+    if(e.matches){
+            
+            $('.port').removeClass('active');
+            $('.aboutTxt dl dd').eq(1).slideUp();
+    }else{
+        $('.aboutTxt dl dd').slideDown();
+    }
+});
+
+
+
+
+
+m.addListener(function(e){
+if(e.matches){
+ $('body').removeClass('control');
+ abilityMob();
+ $('nav div').eq(0).slideUp(0);
+ $('nav').slideDown();
+
+
+//이하
+
+}
+else{
+     
+    $('body').addClass('control');
+
+
+    if($('body').hasClass('control')){
+        idx=0;
+        localControl();
+        abilityDesk();
+     
+    }
+    $('.bugger').removeClass('active');
+    $('.bugger').addClass('Hover');
+    $('nav div').eq(0).slideDown(0); //네비 데스크탑일때
+
+    $('.port >div').animate({ // 포트에 서브스크롤이 내려왔을때 데스크탑 환경에서 깨지는걸 방지
+        scrollTop:0
+    },0);
+
+
+
+  //이상
+}
+});
+m1200.addListener(function(e){
+    if(e.matches){
+        $('body').removeClass('controlPort');
+    }else{
+        $('body').addClass('controlPort');
+    }
+});
+
+
+
+
+
+
+
+function startJS(){
+	var windowWidth = $( window ).width();
+    if(windowWidth < 860) {
+        $('body').removeClass('control');
+       
+	
+	}else{
+        $('body').addClass('control');
+    
+        
+	}
+
+
+    if(windowWidth < 1200){
+        $('body').removeClass('controlPort');
+    }else{
+        $('body').addClass('controlPort');
+    }
+    
+
+}
+startJS();
+
+
+
+
+
 
    
 })
+
+
+
+
+
+
+
+
+
